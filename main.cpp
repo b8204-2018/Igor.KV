890f8de044d5f9725052d67f36365a8bdceb650e
reload956 <igor.kokorin.992@gmail.com>
2019-05-17 23:12:12 +1000

    REfreshed

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include"classes.h"
 #include"gtest/gtest.h"
+
 using namespace std;
 
+void printSortedArray(Sort &sort, int *arr, int n) {
+    int *res = new int[n];
+    memcpy(res, arr, n * sizeof(int));
+
+    sort.sort(res, n);
+
+    for (int i = 0; i < n; i++) {
+        cout << " " << res[i];
+    }
+}
+
 int main() {
-    int a[] = {5,2,1,7,2};
+    int a[] = {5, 2, 1, 7, 2};
     QuickSort sort1;
     printSortedArray(sort1, a, 5);
 
-    cout<<'q'<<endl;
+    cout << 'q' << endl;
 
-    int b[] = {5,2,1,7,2};
+    int b[] = {5, 2, 1, 7, 2};
     ChoiceSort sort2;
     printSortedArray(sort2, b, 5);
 
-    cout<<'c'<<endl;
+    cout << 'c' << endl;
 
-    int c[] = {5,2,1,7,2};
+    int c[] = {5, 2, 1, 7, 2};
     MergeSort sort3;
     printSortedArray(sort3, c, 5);
     return 0;
